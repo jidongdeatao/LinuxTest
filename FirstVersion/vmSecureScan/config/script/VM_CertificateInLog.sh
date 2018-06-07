df8c668b4f13a50fe906be812dc209cfc2bea9d7
jidongdeatao <972395203@qq.com>
2018-06-07 11:00:13 +0800

    Create VM_CertificateInLog.sh

---
+++ FirstVersion/vmSecureScan/config/script/VM_CertificateInLog.sh
@@ -0,0 +1,7 @@
+key_words="(certificate|auth|session|token|key|ak|sk)(\s*:|\s*=|\"\s*(:|=)|\'\s*(:|=)).*[0-9a-zA-Z]{20,}"
+find /var -name '*.log' -type f | xargs file|grep -E 'text|XML|PC bitmap data'|awk '{print $1}'|sed 's/:$//g'|xargs grep -i -n -E -H "$key_words" | egrep -v 'bash_history|Binary file|containerScan|vmSecureScan|/devicemapper/mnt' 2>/dev/null
+find /var -name '*.dat' -type f | xargs file|grep -E 'text|XML|PC bitmap data'|awk '{print $1}'|sed 's/:$//g'|xargs grep -i -n -E -H "$key_words" | egrep -v 'bash_history|Binary file|containerScan|vmSecureScan|/devicemapper/mnt' 2>/dev/null
+find /var -name '*.trace' -type f | xargs file|grep -E 'text|XML|PC bitmap data'|awk '{print $1}'|sed 's/:$//g'|xargs grep -i -n -E -H "$key_words" | egrep -v 'bash_history|Binary file|containerScan|vmSecureScan|/devicemapper/mnt' 2>/dev/null
+find /opt -name '*.log' -type f | xargs file|grep -E 'text|XML|PC bitmap data'|awk '{print $1}'|sed 's/:$//g'|xargs grep -i -n -E -H "$key_words" | egrep -v 'bash_history|Binary file|containerScan|vmSecureScan|/devicemapper/mnt' 2>/dev/null
+find /opt -name '*.dat' -type f | xargs file|grep -E 'text|XML|PC bitmap data'|awk '{print $1}'|sed 's/:$//g'|xargs grep -i -n -E -H "$key_words" | egrep -v 'bash_history|Binary file|containerScan|vmSecureScan|/devicemapper/mnt' 2>/dev/null
+find /opt -name '*.trace' -type f | xargs file|grep -E 'text|XML|PC bitmap data'|awk '{print $1}'|sed 's/:$//g'|xargs grep -i -n -E -H "$key_words" | egrep -v 'bash_history|Binary file|containerScan|vmSecureScan|/devicemapper/mnt' 2>/dev/null
