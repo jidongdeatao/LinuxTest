9ae85ecfe6e8629623210bdaf7497a739a5b266f
jidongdeatao <972395203@qq.com>
2018-06-07 16:05:14 +0800

    Create VM_JSX_Language_js.sh

---
+++ FirstVersion/vmSecureScan/config/script/VM_JSX_Language_js.sh
@@ -0,0 +1,5 @@
+find / \
+-path /proc -prune -o \
+-path /boot -prune -o \
+-path /tmp -prune -o \
+-type f -name "*.js" -user paas | xargs egrep -n -i '^[[:blank:]]*<\!--|^[[:blank:]]*//|^[[:blank:]]*/\*|^[[:blank:]]*\*' | egrep -v 'bash_history|Binary file|containerScan|vmSecureScan|/devicemapper/mnt' 2>/dev/null
